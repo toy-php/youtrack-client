5cf3ed9323623318a38999756d71e9119facbc25
toy-php <seymus007@gmail.com>
2018-10-26 11:58:13 +0300

    Добавлены сущности

---
+++ src/youtrack/entity/EnumBundleElement.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace youtrack\entity;
+
+use youtrack\core\Entity;
+
+class EnumBundleElement extends Entity
+{
+
+}
---
+++ src/youtrack/entity/VersionBundleElement.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace youtrack\entity;
+
+use youtrack\core\Entity;
+
+class VersionBundleElement extends Entity
+{
+
+}
+
